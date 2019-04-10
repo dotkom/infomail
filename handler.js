8ca3303ae00c19d7dea1bcd18f5ccca190c0e024
Tobias Kongsvik <tokongs98@gmail.com>
2019-04-10 20:30:56 +0200

    project setup

---
+++ handler.js
@@ -0,0 +1,33 @@
+
+let nodemailer = require('nodemailer')
+let aws = require('aws-sdk');
+
+let transporter = nodemailer.createTransport({
+    SES: new aws.SES({
+        apiVersion: '2010-12-01'
+    })
+});
+
+
+function sendMail(from_address, to_address, cc_addresses){
+    console.log('TO: ' + to_address + ', FROM: ' + from_address);
+    const data = {
+        from: from_address,
+        to: to_address,
+        cc: cc_addresses,
+        subject: 'Infomail',
+        text: 'Infomail',
+    }
+
+    transporter.sendMail(data, (err, info) => {
+        console.log(info.envelope);
+        console.log(info.messageId);
+    });
+ }
+
+
+exports.myHandler = function(event, context, callback) {   
+    
+    sendMail()
+    callback(null, "some success message");
+}
